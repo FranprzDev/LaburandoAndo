4f3b473de4d84130c924a38b2c7b87e9f080b77f
Julian <julianser@hotmail.com.ar>
2024-07-31 16:32:03 -0300

    Se hicieron correcciones para que se vea mejor en las vistas de celular y tablet

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,24 +7,24 @@ import { FaInstagramSquare } from "react-icons/fa"
 const Footer = () => {
   return (
     <footer className='bg-body-tertiary'>
-      <Container fluid="md">
+      <Container fluid="md" className='text-center text-md-start'>
         <Row>
-            <Col md={3}>
+            <Col className='mt-3 mt-md-0' md={6} lg={3}>
             <Link to={'/'} className='navbar-brand'><h5 className='display-6'>LaburandoAndo</h5></Link>
             </Col>
-            <Col md={3}>
+            <Col className='mt-3 mt-md-0' md={6} lg={3}>
             <h5>Sobre Nosotros</h5>
             <ul className='list-group'>
                 <Link to={'/'} className='list-group-item border-0 pt-0 ps-0 bg-body-tertiary'>¿Quienes Somos?</Link>
                 <Link to={'/'} className='list-group-item border-0 pt-0 ps-0 bg-body-tertiary'>Nuestros Valores</Link>
             </ul>
-            <article className='mt-5'>
+            <article className='mt-lg-5'>
                 <h5>Síguenos en nuestras redes</h5>
                 <Link><FaFacebook className='fs-1 text-primary'/></Link>
                 <Link><FaInstagramSquare className='fs-1 ms-2 instagramLogo'/></Link>
             </article>
             </Col>
-            <Col md={3}>
+            <Col className='mt-3 mt-md-3 mt-lg-0' md={6} lg={3}>
             <h5>Términos y Condiciones</h5>
             <ul className='list-group'>
                 <Link to={'/'} className='list-group-item border-0 pt-0 ps-0 bg-body-tertiary'>Políticas de privacidad</Link>
@@ -32,7 +32,7 @@ const Footer = () => {
                 <Link to={'/'} className='list-group-item border-0 pt-0 ps-0 bg-body-tertiary'>Condiciones de uso para clientes</Link>
             </ul>
             </Col>
-            <Col md={3}>
+            <Col className='mt-3 mt-md-3 mt-lg-0' md={6} lg={3}>
             <h5>Asistencia</h5>
             <ul className='list-group'>
                 <Link to={'/'} className='list-group-item border-0 pt-0 ps-0 bg-body-tertiary'>¿Necesitas Ayuda?</Link>
@@ -40,6 +40,7 @@ const Footer = () => {
             </ul>
             </Col>
         </Row>
+        <h5 className='text-center my-3'>LaburandoAndo. &copy; Todos los Derechos Reservados. 2024</h5>
       </Container>
     </footer>
   )
