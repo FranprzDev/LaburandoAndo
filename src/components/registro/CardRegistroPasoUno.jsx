cd3cbdc118701b6669a5e1b876cb48ee9c2f3a33
brunomry <brunomadozzo@gmail.com>
2024-08-01 20:56:35 -0300

    modifique px por porcentaje y viewport a ancho y alto respectivamente a tarjetas de registro paso uno y de px a rem a fontsize de texto de parrafos y btn

--- src/components/registro/CardRegistroPasoUno.jsx
+++ src/components/registro/CardRegistroPasoUno.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const CardRegistroPasoUno = ({ usuario }) => {
   return (
     <article className="cardRegister d-flex justify-content-center align-items-center rounded-2 p-3">
-      <h2>
+      <h3>
         {usuario
           ? "Soy cliente"
           : "Soy profesional autónomo en busca de trabajo"}
-      </h2>
+      </h3>
     </article>
   );
 };
