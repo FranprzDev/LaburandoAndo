5a8804acff575e7c7b54713cfdfb5502faeb3ed4
brunomry <brunomadozzo@gmail.com>
2024-08-01 20:17:46 -0300

    modifique nombre de archivo registro.css por registroPasoUno.css. Aplique prettier en css y en componentes de registro paso uno

--- src/components/RegistroPasoUno.jsx
+++ src/components/RegistroPasoUno.jsx
@@ -5,7 +5,9 @@ import CardRegistroPasoUno from "./registro/CardRegistroPasoUno";
 const RegistroPasoUno = () => {
   return (
     <section className="container registerStepOne mainSection py-3 py-md-5 text-center">
-      <h1 className="mb-3 mb-md-5">¿Eres un cliente o un profesional autónomo?</h1>
+      <h1 className="mb-3 mb-md-5">
+        ¿Eres un cliente o un profesional autónomo?
+      </h1>
       <p className="text-center fontSizeText">Selecciona una opción</p>
       <div className="d-flex flex-column flex-md-row justify-content-center gap-2 gap-md-5 align-items-center mb-4">
         <CardRegistroPasoUno usuario={true}></CardRegistroPasoUno>
@@ -13,7 +15,12 @@ const RegistroPasoUno = () => {
       </div>
       <div className="mb-5">
         <p className="fontSizeText">¿Ya tienes cuenta?</p>
-        <Link to={"/login"} className="btn px-5 border btnOptionLogin fontSizeText text-white">Ingresar</Link>
+        <Link
+          to={"/login"}
+          className="btn px-5 border btnOptionLogin fontSizeText text-white"
+        >
+          Ingresar
+        </Link>
       </div>
     </section>
   );
