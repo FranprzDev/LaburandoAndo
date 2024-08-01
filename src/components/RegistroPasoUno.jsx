ec9533a038487b77897ac0d80ccaba579dae38c0
brunomry <brunomadozzo@gmail.com>
2024-08-01 10:32:58 -0300

    agregue cp RegistroPasoUno colocando seccion con titulo y parrafo, y su ruta en cp AppRouter

---
+++ src/components/RegistroPasoUno.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegistroPasoUno = () => {
+    return (
+        <section className='container mainSection py-5 text-center'>
+            <h1 className='mb-5'>¿Eres un cliente o un profesional autónomo?</h1>
+            <p className='text-center'>Selecciona una opción</p>
+        </section>
+    );
+};
+
+export default RegistroPasoUno;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Menu from '../components/Menu'
 import Footer from '../components/Footer'
+import RegistroPasoUno from '../components/RegistroPasoUno'
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
     <Menu/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/registropasouno' element={<RegistroPasoUno></RegistroPasoUno>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
