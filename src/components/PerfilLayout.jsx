0d19fe08728115647af1c3921df4f14a70f28f13
brunomry <brunomadozzo@gmail.com>
2024-08-02 15:37:54 -0300

    agregue navbar offcanvas de bootstrap en cp MenuProfesional y agregue estilos css para que ocupe todo el viewport height

--- src/components/PerfilLayout.jsx
+++ src/components/PerfilLayout.jsx
@@ -2,9 +2,9 @@ import MenuProfesional from "./profesional/MenuProfesional";
 
 const PerfilLayout = ({ children }) => {
   return (
-    <section className="px-0 container-fluid mainSection">
-      <div className="row containerProfile w-100">
-        <article className="col-md-4 col-lg-3 col-xxl-2 h-100">
+    <section className="container-fluid mainSection">
+      <div className="row containerProfile">
+        <article className="col-md-4 col-lg-3 col-xxl-2 px-0">
           <MenuProfesional></MenuProfesional>
         </article>
         <article className="col-md-8 col-lg-9 col-xxl-10">{children}</article>
--- src/components/profesional/MenuProfesional.jsx
+++ src/components/profesional/MenuProfesional.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
+import React from "react";
+import "../../styles/perfilProfesional.css";
+import { Link } from "react-router-dom";
 
 const MenuProfesional = () => {
-    return (
-        <div>
-            
+  return (
+    <nav className="navbar navbar-expand-md menuProfessional bg-body-tertiary d-flex align-items-start w-100 px-2">
+      <div className="px-0 container-fluid containerMenu d-flex flex-md-column align-items-start">
+        <Link className="navbar-brand" to={"/"}>
+          LaburandoAndo
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="offcanvas offcanvas-start"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
+          <div className="offcanvas-header">
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end d-flex flex-column flex-grow-1 pe-3">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to={"/"}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li></li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </div>
+    </nav>
+  );
 };
 
 export default MenuProfesional;
