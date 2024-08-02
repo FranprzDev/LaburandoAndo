87fec65d1a10c5c5ab4113d2ef88793ec5e6864c
brunomry <brunomadozzo@gmail.com>
2024-08-02 16:13:41 -0300

    agregue logo, imagen, nombre de usuario, ubicacion y opciones de menu en navbar offcanvas de cp MenuProfesional

--- src/components/profesional/MenuProfesional.jsx
+++ src/components/profesional/MenuProfesional.jsx
@@ -1,77 +1,74 @@
 import React from "react";
 import "../../styles/perfilProfesional.css";
 import { Link } from "react-router-dom";
+import image from "../../img/Logo.jpg";
 
 const MenuProfesional = () => {
   return (
     <nav className="navbar navbar-expand-md menuProfessional bg-body-tertiary d-flex align-items-start w-100 px-2">
-      <div className="px-0 container-fluid containerMenu d-flex flex-md-column align-items-start">
-        <Link className="navbar-brand" to={"/"}>
-          LaburandoAndo
-        </Link>
+      <div className="px-0 px-md-2 py-md-3 container-fluid containerMenu d-flex flex-md-column align-items-start">
+        <div className="text-md-center">
+          <Link className="navbar-brand fs-2" to={"/"}>
+            LaburandoAndo
+          </Link>
+        </div>
+        <figure className="d-none d-md-block text-center mx-auto w-100 mt-5">
+          <img
+            src={image}
+            alt=""
+            width={100}
+            className="rounded-circle shadow"
+          />
+          <figcaption className="fw-bold fs-5">Bruno Madozzo Romay</figcaption>
+          <figcaption>Tucumán, Argentina</figcaption>
+        </figure>
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="offcanvas"
           data-bs-target="#offcanvasNavbar"
-          aria-controls="offcanvasNavbar"
-          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div
-          className="offcanvas offcanvas-start"
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-        >
+        <div className="offcanvas offcanvas-start w-100" id="offcanvasNavbar">
           <div className="offcanvas-header">
             <button
               type="button"
               className="btn-close"
               data-bs-dismiss="offcanvas"
-              aria-label="Close"
             ></button>
           </div>
-          <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end d-flex flex-column flex-grow-1 pe-3">
+          <div className="offcanvas-body d-flex justify-content-center w-100 px-4">
+            <ul className="navbar-nav d-flex flex-column justify-content-center w-100 mt-3">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to={"/"}>
+                <Link className="nav-link w-100" aria-current="page" to={"/"}>
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
+              <li className="nav-item mt-xl-3">
+                <Link className="nav-link w-100" to={"/miperfil"}>
+                  Mi Perfil
+                </Link>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li></li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/publicar"}>
+                  Publicar Anuncio
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="#">
+                  Mis Publicaciones
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="#">
+                  Ayuda
+                </Link>
+              </li>
+              <li className="nav-item mt-xl-3">
+                <Link className="nav-link" href="#">
+                  Cerrar Sesión
+                </Link>
               </li>
             </ul>
           </div>
