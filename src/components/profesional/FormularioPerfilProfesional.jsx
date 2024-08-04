76bc7833c5d0f80b441da13266c225dc80efdad6
brunomry <brunomadozzo@gmail.com>
2024-08-04 15:20:45 -0300

    agregue cp Form de react-bootstrap en cp FormularioPerfilProfesional con los campos nombre y apellido, email, ubicacion, nro de whatsapp e imagen de perfil, ademas de btn para subir img y btn para editar los datos

--- src/components/profesional/FormularioPerfilProfesional.jsx
+++ src/components/profesional/FormularioPerfilProfesional.jsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import { Form } from "react-bootstrap";
 
-const FormularioPerfilProfesional = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const FormularioPerfilProf = () => {
+  return (
+    <Form>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="name" className="">
+            Nombre y Apellido
+          </Form.Label>
+          <Form.Control
+            type="text"
+            id="name"            
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="email" className="">
+            Email
+          </Form.Label>
+          <Form.Control
+            type="email"
+            id="email"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="ubication" className="">
+            Ubicación
+          </Form.Label>
+          <Form.Control
+            type="text"
+            id="ubication"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="wpp" className="">
+            Número de WhatsApp
+          </Form.Label>
+          <Form.Control
+            type="text"
+            id="wpp"
+          />
+        </Form.Group>
+        <Form.Group className="">
+          <Form.Label htmlFor="wpp" className="">
+            Foto de Perfil
+          </Form.Label>
+          <img
+            src=""
+            alt=""
+          />
+          <Form.Control type="file" id="file" />
+          <button className="">
+            Subir Imagen
+          </button>
+        </Form.Group>   
+      <div className="">
+        <button type="submit" className="">
+          Editar Mis datos
+        </button>
+      </div>
+    </Form>
+  );
 };
 
-export default FormularioPerfilProfesional;
+export default FormularioPerfilProf;
