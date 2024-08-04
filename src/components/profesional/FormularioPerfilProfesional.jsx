44f3de4c3d58e0295cee50ce61df19d3427d549d
brunomry <brunomadozzo@gmail.com>
2024-08-04 15:26:24 -0300

    aplique grilla en formulario de cp FormularioPerfilProfesional y estilice con bootstrap a inputs, labels, img de perfil y botones

--- src/components/profesional/FormularioPerfilProfesional.jsx
+++ src/components/profesional/FormularioPerfilProfesional.jsx
@@ -2,62 +2,72 @@ import { Form } from "react-bootstrap";
 
 const FormularioPerfilProf = () => {
   return (
-    <Form>
+    <Form className="row gy-3 mt-2 mt-md-5 bg-white shadow rounded-2 px-3 px-xl-5 pb-3 mt-lg-2 pt-4 mt-xl-5 border">
+      <div className="col-lg-7">
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="name" className="">
+          <Form.Label htmlFor="name" className="text-md-center w-100 text-lg-start fw-bold mb-0 ">
             Nombre y Apellido
           </Form.Label>
           <Form.Control
             type="text"
-            id="name"            
+            id="name"
+            className="px-0 border-start-0 border-end-0 border-top-0 border-bottom rounded-0" 
           />
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="email" className="">
+          <Form.Label htmlFor="email" className="text-md-center w-100 text-lg-start fw-bold mb-0 ">
             Email
           </Form.Label>
           <Form.Control
             type="email"
             id="email"
+            className="px-0 border-start-0 border-end-0 border-top-0 border-bottom rounded-0"
           />
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="ubication" className="">
+          <Form.Label htmlFor="ubication" className="text-md-center w-100 text-lg-start fw-bold mb-0 ">
             Ubicación
           </Form.Label>
           <Form.Control
             type="text"
             id="ubication"
+            className="px-0 border-start-0 border-end-0 border-top-0 border-bottom rounded-0"
           />
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="wpp" className="">
+          <Form.Label htmlFor="wpp" className="text-md-center w-100 text-lg-start fw-bold mb-0 ">
             Número de WhatsApp
           </Form.Label>
           <Form.Control
             type="text"
             id="wpp"
+            className="px-0 border-start-0 border-end-0 border-top-0 border-bottom rounded-0"
           />
         </Form.Group>
-        <Form.Group className="">
-          <Form.Label htmlFor="wpp" className="">
+      </div>
+      <div className="col-lg-5 mt-lg-5">
+        <Form.Group className="mb-3 d-flex flex-column justify-content-center align-items-md-center gap-2">
+          <Form.Label htmlFor="wpp" className="text-md-center w-100 fw-bold mb-0 ">
             Foto de Perfil
           </Form.Label>
           <img
             src=""
             alt=""
+            className="border rounded-circle"
           />
-          <Form.Control type="file" id="file" />
-          <button className="">
+          <Form.Control type="file" className="d-none" id="file" />
+          <button className="btn rounded-2 btn-secondary px-4">
             Subir Imagen
           </button>
-        </Form.Group>   
-      <div className="">
-        <button type="submit" className="">
+        </Form.Group>
+      </div>
+      <div className="d-flex justify-content-end justify-content-md-center justify-content-lg-end d-block w-100">
+        <button type="submit" className="px-3 px-md-5 py-2 rounded-2 text-white border-0">
           Editar Mis datos
         </button>
       </div>
     </Form>
+
   );
 };
 
