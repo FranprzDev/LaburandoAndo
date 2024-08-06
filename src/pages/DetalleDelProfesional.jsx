493686c7c9d6431c0dafdc68e25dd60652278317
CosmeNicolas <nicomas7590@gmail.com>
2024-08-05 23:20:41 -0300

    agregue la card donde ira la foto del profesional con su descripcion y boton de contacto diracto de whatsapp

--- src/pages/DetalleDelProfesional.jsx
+++ src/pages/DetalleDelProfesional.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import logo from '../img/Logo.jpg';
+import '../styles/estiloDetalleDelProfesional.css'
+import { FaStar } from "react-icons/fa";
+import Button from 'react-bootstrap/Button';
+import { FaWhatsapp } from "react-icons/fa";
 
 const DetalleDelProfesional = () => {
   return (
-    <div>DetalleDelProfesional</div>
+    <section className='d-flex justify-content-evenly mb-4'>
+      <Card className='me-3 mt-3 card-detalle'  style={{ width: '18rem' }}>
+      <Card.Body className='d-flex flex-column justify-content-center align-items-center'>
+        <Image src={logo} roundedCircle  className='img-circulo-card '/>
+        <Card.Title >Juan Perez</Card.Title>
+        <Card.Text>
+          Yerba Buena, Tucumán
+        </Card.Text> 
+        <Card.Subtitle className="mb-2 text-muted">
+          <div className=' d-flex'>
+          <FaStar className='me-2 text-warning' />
+          <p>4.0</p>
+          </div>
+          <p>10 Valoraciones</p>
+          </Card.Subtitle>
+          <Button variant="success">
+            <FaWhatsapp className='me-2' />
+            Contactar
+          </Button>
+      </Card.Body>
+    </Card>
+    <div className='mt-3'>
+    <Button variant='danger' className='btn-descripcion-profesional opacity-50'  size="md">
+          Profesor particular
+        </Button>
+        <h2 className='my-2'>Clases Particulares</h2>
+        <h3>Descripción del anuncio</h3>
+        <hr />
+        <p>Hola me llamo Juan Perez y tengo 25 años. <br />Brindo clases a alumnos que deseen capacitarse en inglés</p>
+    </div>
+    </section>
+
   )
 }
 
 export default DetalleDelProfesional
+
