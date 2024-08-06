57fde4ef9179e0b46c0838efab3e5e64bff9ed21
Flavia Walther <flamwalther@gmail.com>
2024-08-06 15:43:36 -0300

    se agrega boton conectar con Google

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       {/* <h1>hola</h1> */}
+
       <RegitroProfesional />
     </>
   );
--- src/pages/RegitroProfesional.jsx
+++ src/pages/RegitroProfesional.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-
 import { useForm } from "react-hook-form";
+import "../styles/login.css";
 
 const RegitroProfesional = () => {
   const {
@@ -131,14 +131,38 @@ const RegitroProfesional = () => {
               </fieldset>
 
               <div className="d-grid">
-                <button type="submit" className="btn btn-danger">
-                  Registrarme
+                <button type="submit" className="btn btn-danger btn-lg ">
+                  Registrar
+                </button>
+              </div>
+
+              <div>
+                <button className="btn btn-lg btn-LoginGoogle btn-outline-secondary w-100 d-flex align-items-center justify-content-center mt-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="me-2"
+                  >
+                    <circle cx="12" cy="12" r="10" />
+                    <circle cx="12" cy="12" r="4" />
+                    <line x1="21.17" x2="12" y1="8" y2="8" />
+                    <line x1="3.95" x2="8.54" y1="6.06" y2="14" />
+                    <line x1="10.88" x2="15.46" y1="21.94" y2="14" />
+                  </svg>
+                  Continúa con Google
                 </button>
               </div>
 
               <div className="d-grid mt-5">
                 <p>¿Ya tienes una cuena?</p>
-                <button className="btn btn-secondary">Ingresar</button>
+                <button className="btn btn-secondary btn-lg">Ingresar</button>
               </div>
             </form>
           </div>
