61184abcaccda9ab2af30f6ef15c3f29a5712d0d
Francisco Perez <franutn23@gmail.com>
2024-08-07 03:43:40 -0300

    Añado la lógica al router, acomodandolo y también añado el pasamanos de datos entre los distintas páginas del registro.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,56 +1,58 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
-import RegistroPasoUno from '../components/RegistroPasoUno'
-import DetalleDelProfesional from '../pages/DetalleDelProfesional'
-import RegistroPasoTres from '../pages/RegistroPasoTres'
-import PerfilLayout from "../components/PerfilLayout";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "../components/MainLayout";
+import Home from "../pages/Home";
+import RegistroPasoUno from "../pages/AuthPages/RegistroPasoUno";
+import DetalleDelProfesional from "../pages/DetalleDelProfesional";
+import RegistroPasoTres from "../pages/AuthPages/RegistroPasoTres";
+import PerfilLayout from "../components/PerfilLayout";
 import PublicacionProfesional from "../components/profesional/PublicacionProfesional";
 import PerfilProfesional from "../components/profesional/PerfilProfesional";
-
+import Login from "../pages/AuthPages/Login";
+import { RegisterProvider } from "../Context/registerContext";
+import RegistroPasoDos from "../pages/AuthPages/RegistroPasoDos";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/detalleDelProfesional' element={<DetalleDelProfesional/>}/>
-        <Route path='/registropasouno' element={<RegistroPasoUno></RegistroPasoUno>}/>
-      <Route 
-          path='/' 
-          element={
-            <MainLayout>
-              <Home />
-            </MainLayout>
-          } 
-        />
-        <Route
-          path="/mi-perfil"
-          element={
-            <PerfilLayout>
-              <PerfilProfesional></PerfilProfesional>
-            </PerfilLayout>
-          }
-        />
-        <Route
-          path="/publicar-anuncio"
-          element={
-            <PerfilLayout>
-              <PublicacionProfesional></PublicacionProfesional>
-            </PerfilLayout>
-          }
-        />
-        <Route path='/registropasouno' element={
-          <MainLayout>
-            <RegistroPasoUno></RegistroPasoUno>
-          </MainLayout>
-          }/>
-        <Route path='/registropasotres' element={
-          <MainLayout>
-            <RegistroPasoTres></RegistroPasoTres>
-          </MainLayout>
-          }/>
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+
+            <Route path="/register" element={<RegisterProvider><RegistroPasoUno /></RegisterProvider>} />
+            <Route path="/register2" element={<RegisterProvider><RegistroPasoDos /></RegisterProvider>} />
+            <Route path="/register3" element={<RegisterProvider><RegistroPasoTres /></RegisterProvider>} />
+
+          {/* <Route
+            path="/mi-perfil"
+            element={
+              <PerfilLayout>
+                <PerfilProfesional></PerfilProfesional>
+              </PerfilLayout>
+            }
+          />
+          <Route
+            path="/publicar-anuncio"
+            element={
+              <PerfilLayout>
+                <PublicacionProfesional></PublicacionProfesional>
+              </PerfilLayout>
+            }
+          />
+          <Route
+            path="/registropasouno"
+            element={<RegistroPasoUno></RegistroPasoUno>}
+          />
+          <Route
+            path="/registropasotres"
+            element={<RegistroPasoTres></RegistroPasoTres>}
+          /> */}
+
+          {/* <Route path="/registropasotres" element={<RegistroPasoTres />} /> */}
+          {/* <Route path="/login" element={<Login />} /> */}
+        </Routes>
+      </MainLayout>
     </BrowserRouter>
   );
 };
