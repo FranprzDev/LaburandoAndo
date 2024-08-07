134ce6808101da3fc1430d35c89a2bcb05b62d03
CosmeNicolas <nicomas7590@gmail.com>
2024-08-06 21:09:51 -0300

    agregue el maquetado responsive y las opiniones de los usuarios

--- src/pages/DetalleDelProfesional.jsx
+++ src/pages/DetalleDelProfesional.jsx
@@ -5,11 +5,15 @@ import '../styles/estiloDetalleDelProfesional.css'
 import { FaStar } from "react-icons/fa";
 import Button from 'react-bootstrap/Button';
 import { FaWhatsapp } from "react-icons/fa";
+import { Container } from 'react-bootstrap';
+import Opiniones from '../components/Opiniones';
 
 const DetalleDelProfesional = () => {
   return (
-    <section className='d-flex justify-content-evenly mb-4'>
-      <Card className='me-3 mt-3 card-detalle'  style={{ width: '18rem' }}>
+    <Container >
+    <section className='d-flex mb-4 justify-content-center contenedor-detalle'>
+      <Card className='me-3 mt-3 card-detalle'  >
+      <div>
       <Card.Body className='d-flex flex-column justify-content-center align-items-center'>
         <Image src={logo} roundedCircle  className='img-circulo-card '/>
         <Card.Title >Juan Perez</Card.Title>
@@ -28,6 +32,7 @@ const DetalleDelProfesional = () => {
             Contactar
           </Button>
       </Card.Body>
+      </div>
     </Card>
     <div className='mt-3'>
     <Button variant='danger' className='btn-descripcion-profesional opacity-50'  size="md">
@@ -39,7 +44,8 @@ const DetalleDelProfesional = () => {
         <p>Hola me llamo Juan Perez y tengo 25 años. <br />Brindo clases a alumnos que deseen capacitarse en inglés</p>
     </div>
     </section>
-
+    <Opiniones/>
+    </Container>
   )
 }
 
