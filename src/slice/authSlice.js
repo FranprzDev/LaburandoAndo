f8ed3a91771cf263a349e5a64d5ec80673e9b3f3
Julian <julianser@hotmail.com.ar>
2024-08-07 01:30:35 -0300

    Se creo la estructura basica de Redux Toolkit

---
+++ src/slice/authSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers:{
+
+    },
+    extraReducers: (builder) =>{}
+})
+
+export default authSlice.reducer
