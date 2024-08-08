20e8464587b083bfcfade8aa9e280ac05aefcac8
Flavia Walther <flamwalther@gmail.com>
2024-08-08 08:45:30 -0300

    maquetado de page Sobre Nosotros

---
+++ src/pages/Nosotros.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import "../styles/Nosotros.css";
+
+const Nosotros = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row mb-5 text-center">
+          <h1>Quienes somos?</h1>
+          <div className="col-md-6 align-items-center mt-5">
+            <h6>aaaaaaaaaaaaaa</h6>
+          </div>
+
+          <div className="col-md-6 align-items-center mt-5">
+            <h6>gggggggggggggggg</h6>
+          </div>
+        </div>
+
+        <div className="row text-center mb-5">
+          <h1>Nuestros Principios</h1>
+
+          <div className="row row-cols-1 row-cols-md-2 g-4 justify-content-center">
+            <div className="col">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h2 className="card-title mb-4">Misión</h2>
+                  <p className="card-text">vvvvvvvvvvvv</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h2 className="card-title mb-4">Visión</h2>
+                  <p className="card-text">kkkkkkkkkkkkkk</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row text-center ">
+          <h1>Fundadores</h1>
+
+          <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center card-fundadores">
+            <div className="col">
+              <div className="card h-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h3 className="card-title">nombre y apellido</h3>
+                </div>
+                <div className="card-footer">
+                  <small className="text-body-secondary">links</small>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card h-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h3 className="card-title">nombre y apellido</h3>
+                </div>
+                <div className="card-footer">
+                  <small className="text-body-secondary">links</small>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card h-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h3 className="card-title">nombre y apellido</h3>
+                </div>
+                <div className="card-footer">
+                  <small className="text-body-secondary">links</small>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card h-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h3 className="card-title">nombre y apellido</h3>
+                </div>
+                <div className="card-footer">
+                  <small className="text-body-secondary">links</small>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card h-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h3 className="card-title">nombre y apellido</h3>
+                </div>
+                <div className="card-footer">
+                  <small className="text-body-secondary">links</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Nosotros;
