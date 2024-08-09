c6c8d74667b366e1840e8d8a0e988fdf8152666a
Francisco Perez <franutn23@gmail.com>
2024-08-09 19:44:20 -0300

    fix the logical login

--- src/pages/AuthPages/Login.jsx
+++ src/pages/AuthPages/Login.jsx
@@ -1,11 +1,11 @@
 import "../../styles/login.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { fetchWorkers } from "../../slice/workersSlice";
 import { loginUser } from "../../slice/authSlice";
+import { reset } from "../../slice/authSlice";
 
 export default function Login() {
   const {
@@ -15,139 +15,126 @@ export default function Login() {
   } = useForm();
 
   const dispatch = useDispatch();
-  const workers = useSelector((state)=> state.workers.workers);
-  const workersStatus = useSelector((state)=> state.workers.status)
-
   const navigate = useNavigate();
+  const stateSync = useSelector((state) => state.auth.stateSync);
   
   useEffect(() => {
-    if (workersStatus === 'idle') {
-      dispatch(fetchWorkers());
-    }
-  }, []);
-
-  const login = async (usuario) => {
-
-    const trabajadorBuscado = workers.find(
-      (w) => w.mail === usuario.mail
-    ); 
-
-    
-    if(trabajadorBuscado){
-
-      dispatch(loginUser(usuario))
+    if (stateSync === 'exitoso') {
       navigate("/mi-perfil");
       Swal.fire({
-        icon: 'success',
-        title: 'Bienvenido'
+        icon: "success",
+        title: "Bienvenido",
       });
-    }else{
+      dispatch(reset());
+    } else if (stateSync === 'error') {
       Swal.fire({
-        icon: 'error',
-        title: 'No se pudo iniciar sesión',
-        text: 'Los datos ingresados no son correctos.',
+        icon: "error",
+        title: "No se pudo iniciar sesión",
+        text: "Los datos ingresados no son correctos.",
       });
     }
+  }, [stateSync, navigate]);  
+
+  const login = async (usuario) => {
+      dispatch(loginUser(usuario));
   };
 
   return (
-        <div className="container d-flex flex-column align-items-center justify-content-center py-4">
-        
-        <div className="row w-100">
-          
-          <div className="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto">
-            <form className="card p-4" onSubmit={handleSubmit(login)}>
-              <div className="text-center">
-                <h5 className="card-title">
-                  Accede a <span className="text-primary">LaburandoAndo</span>
-                </h5>
-              </div>
-              <div className="card-body">
-                <button className="btn btn-LoginGoogle btn-outline-secondary w-100 d-flex align-items-center justify-content-center mb-4">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="me-2"
-                  >
-                    <circle cx="12" cy="12" r="10" />
-                    <circle cx="12" cy="12" r="4" />
-                    <line x1="21.17" x2="12" y1="8" y2="8" />
-                    <line x1="3.95" x2="8.54" y1="6.06" y2="14" />
-                    <line x1="10.88" x2="15.46" y1="21.94" y2="14" />
-                  </svg>
-                  Continúa con Google
-                </button>
-                <div className="text-center my-3">
-                  <div className="mb-3">
-                    <input
-                      type="email"
-                      className="form-control input"
-                      placeholder="juanperez@gmail.com"
-                      {...register("mail", {
-                        required: 'El correo es obligatorio',
-                        pattern: {
-                          value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                          message: "Ingrese una direccion de correo válida",
-                        },
-                      })}
-                    />
-                    <div className="text-danger text-start">
-                      {errors.mail?.message}
-                    </div>
-                  </div>
-                  <div className="mb-3">
-                    <input
-                      type="password"
-                      className="form-control input"
-                      placeholder="Ju4n23!"
-                      {...register("password", {
-                        required: 'La contraseña es obligatoria',
-                        minLength: {
-                          value: 8,
-                          message: 'La clave debe tener al menos 8 caracteres',
-                        },
-                        maxLength: {
-                          value: 70,
-                          message: 'La clave debe tener como máximo 70 caracteres',
-                        },
-                      })}
-                    />
-                     <div className="text-danger text-start">
-                      {errors.password?.message}
-                    </div>
-                  </div>
-                  <button className="btn btn-danger w-100 mb-3">Ingresar</button>
-                </div>
-                <div className="text-center">
-                  <Link to={"/*"} className="text-primary text-decoration-none">
-                    ¿No recuerdas tu contraseña?
-                  </Link>
-                </div>
-              </div>
-              <div className="d-flex align-items-center w-100 my-3">
-                <hr className="flex-grow-1 border border-secondary border-1" />
-                <span className="mx-2">o</span>
-                <hr className="flex-grow-1 border border-secondary border-1" />
-              </div>
-              <div className="text-center">
-                <Link
-                  to={"../register"}
-                  className="btn btn-outline-secondary btn-Registrarse w-100 mt-2 text-dark"
+    <div className="container d-flex flex-column align-items-center justify-content-center py-4">
+      <div className="row w-100">
+        <div className="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto">
+          <form className="card p-4" onSubmit={handleSubmit(login)}>
+            <div className="text-center">
+              <h5 className="card-title">
+                Accede a <span className="text-primary">LaburandoAndo</span>
+              </h5>
+            </div>
+            <div className="card-body">
+              <button className="btn btn-LoginGoogle btn-outline-secondary w-100 d-flex align-items-center justify-content-center mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="me-2"
                 >
-                  Crear una cuenta nueva
+                  <circle cx="12" cy="12" r="10" />
+                  <circle cx="12" cy="12" r="4" />
+                  <line x1="21.17" x2="12" y1="8" y2="8" />
+                  <line x1="3.95" x2="8.54" y1="6.06" y2="14" />
+                  <line x1="10.88" x2="15.46" y1="21.94" y2="14" />
+                </svg>
+                Continúa con Google
+              </button>
+              <div className="text-center my-3">
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    className="form-control input"
+                    placeholder="juanperez@gmail.com"
+                    {...register("mail", {
+                      required: "El correo es obligatorio",
+                      pattern: {
+                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                        message: "Ingrese una direccion de correo válida",
+                      },
+                    })}
+                  />
+                  <div className="text-danger text-start">
+                    {errors.mail?.message}
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    className="form-control input"
+                    placeholder="Ju4n23!"
+                    {...register("password", {
+                      required: "La contraseña es obligatoria",
+                      minLength: {
+                        value: 8,
+                        message: "La clave debe tener al menos 8 caracteres",
+                      },
+                      maxLength: {
+                        value: 70,
+                        message:
+                          "La clave debe tener como máximo 70 caracteres",
+                      },
+                    })}
+                  />
+                  <div className="text-danger text-start">
+                    {errors.password?.message}
+                  </div>
+                </div>
+                <button className="btn btn-danger w-100 mb-3">Ingresar</button>
+              </div>
+              <div className="text-center">
+                <Link to={"/*"} className="text-primary text-decoration-none">
+                  ¿No recuerdas tu contraseña?
                 </Link>
               </div>
-            </form>
-          
-          </div>
+            </div>
+            <div className="d-flex align-items-center w-100 my-3">
+              <hr className="flex-grow-1 border border-secondary border-1" />
+              <span className="mx-2">o</span>
+              <hr className="flex-grow-1 border border-secondary border-1" />
+            </div>
+            <div className="text-center">
+              <Link
+                to={"../register"}
+                className="btn btn-outline-secondary btn-Registrarse w-100 mt-2 text-dark"
+              >
+                Crear una cuenta nueva
+              </Link>
+            </div>
+          </form>
         </div>
-      </div>  
+      </div>
+    </div>
   );
 }
--- src/slice/authSlice.js
+++ src/slice/authSlice.js
@@ -1,39 +1,67 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import instance from "../api/api";
+import { useDispatch } from "react-redux";
 
 const initialState = {
     user: null,
+    stateSync: "iddle",
     state: "iddle",
     error: null
 };
 
 export const loginUser = createAsyncThunk('auth', async(usuario) => {
-    console.log(usuario)
+  try {
     const response = await instance.post('/auth/local/login', usuario);
-    console.log(response.status)
+    
     return response.data.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
 })
 
 const authSlice = createSlice({
     name: 'login',
     initialState,
     reducers:{
+        reset: (state) => {
+          state.stateSync = "iddle";
+          state.error = null;
+        },
+        pending: (state) => {
+          state.stateSync = "cargando";
+        },
+        success: (state) => {
+          state.stateSync = "exitoso";
+        },
+        error: (state, action) => {
+          state.stateSync = "error";
+          state.error = action.payload;
+        },
+        logout: (state) => {
+          state.stateSync = "iddle";
+          sessionStorage.removeItem("usuarioLogeado");
+        },
     },
     extraReducers: (builder) =>{
         builder
         .addCase(loginUser.pending, (state) => {
+          state.stateSync = 'cargando';
           state.status = 'cargando';
         })
         .addCase(loginUser.fulfilled, (state, action) => {
+          state.stateSync = 'exitoso';
           state.status = 'exitoso';
           state.user = action.payload;
           sessionStorage.setItem("usuarioLogeado", JSON.stringify(action.payload))
         })
         .addCase(loginUser.rejected, (state, action) => {
-          state.status = 'denegado';
-          state.error = action.error.message;
+          state.stateSync = 'error';
+          state.status = 'iddle';
+          state.error = action.payload ? action.payload.message : action.error.message;
         });
     }
 })
+
+export const { reset, pending, success, error, logout } = authSlice.actions;
 
 export default authSlice.reducer;
