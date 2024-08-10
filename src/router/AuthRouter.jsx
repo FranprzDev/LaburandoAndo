246a38968c0d4ef01afddf840c301814e7dab55f
Francisco Perez <franutn23@gmail.com>
2024-08-10 06:04:17 -0300

    Add layout by the app router

--- src/router/AuthRouter.jsx
+++ src/router/AuthRouter.jsx
@@ -4,16 +4,19 @@ import RegistroPasoUno from "../pages/AuthPages/RegistroPasoUno";
 import RegistroPasoDos from "../pages/AuthPages/RegistroPasoDos";
 import RegistroPasoTres from "../pages/AuthPages/RegistroPasoTres";
 import Login from "../pages/AuthPages/Login";
+import MainLayout from '../components/MainLayout';
 
 const AuthRouter = () => {
     return (
         <RegisterProvider>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<RegistroPasoUno />} />
-                <Route path="/register2" element={<RegistroPasoDos />} />
-                <Route path="/register3" element={<RegistroPasoTres />} />
-            </Routes>
+            <MainLayout>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<RegistroPasoUno />} />
+                    <Route path="/register2" element={<RegistroPasoDos />} />
+                    <Route path="/register3" element={<RegistroPasoTres />} />
+                </Routes>
+            </MainLayout>
         </RegisterProvider>
     );
 };
--- src/router/ClientRouter.jsx
+++ src/router/ClientRouter.jsx
@@ -1,5 +1,19 @@
-const ClientRouter = () => {
+import Home from '../pages/Home';
+import Profesionales from '../pages/Profesionales';
+import DetalleDelProfesional from '../pages/DetalleDelProfesional';
+import { Routes, Route } from 'react-router-dom';
+import PerfilLayout from '../components/PerfilLayout';
 
+const ClientRouter = () => {
+    return (
+        <PerfilLayout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path='/profesionales' element={<Profesionales/>}/>
+                <Route path="/detalle" element={<DetalleDelProfesional />} />
+            </Routes>
+        </PerfilLayout>
+    )
 }
 
 export default ClientRouter;
