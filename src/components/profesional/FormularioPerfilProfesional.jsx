c23799c833bec48a47e6bf5817963bcc886cfa2a
Bruno Madozzo Romay <108587068+brunomry@users.noreply.github.com>
2024-08-11 02:47:38 -0300

    Logica read perfil profesional (#33)
    * inicialice objeto usuario con sessionStorage o null en auth slice. Corregi ruta de navigate en fn login de cp Login. Mostre datos del profesional en menu lateral y en form de su perfil usando react-hook-form y agregando validaciones a dicho form
    
    * agregue fn cerrar sesion en cp MenuProfesional
    
    * agregue mje de confirmacion de sweetalert en fn cerrar sesion en cp MenuProfesional
    
    * agregue logica para obtener profesional por id en workerSlice y corregi carga de datos en useEffect en cp FormularioPerfilProfesional
    
    * agregue useEffect para obtener profesional por id de workerSlice y mostrar nombre, ubicacion e img en menu lateral de cp MenuProfesional

--- src/components/profesional/FormularioPerfilProfesional.jsx
+++ src/components/profesional/FormularioPerfilProfesional.jsx
@@ -1,12 +1,58 @@
 import { Form } from "react-bootstrap";
 import imgUsuario from "../../img/imgUsuario.png";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { fetchWorkerForID } from "../../slice/workersSlice";
+import { useNavigate } from "react-router-dom";
 
 const FormularioPerfilProf = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  const [habilitado, setHabilitado] = useState(true); //para manejar disabled de inputs y botones, sirve para fn de editar
+
+  const user = useSelector((state) => state.auth.user);
+  const worker = useSelector((state) => state.workers.worker);
+  const status = useSelector((state) => state.workers.status);
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchWorkerForID(user._id));
+    } else {
+      navigate("/");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (worker && status === "exitoso") {
+      cargarDatosUsuario();
+    }
+  }, [worker, status]);
+
+  const cargarDatosUsuario = () => {
+    setValue("fullname", worker.fullname);
+    setValue("mail", worker.mail);
+    setValue("phone", worker.phone);
+    setValue("address", worker.address);
+    setValue("img", worker.img);
+  };
+
   return (
     <Form className="formProfile row gy-3 mt-2 mt-md-5 bg-white shadow rounded-2 px-3 px-xl-5 pb-3 mt-lg-2 pt-4 mt-xl-5 border">
       <div className="col-lg-7">
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="name" className="text-md-center w-100 text-lg-start fw-bold mb-0 fontSizeLabel">
+          <Form.Label
+            htmlFor="name"
+            className="text-md-center w-100 text-lg-start fw-bold mb-0 fontSizeLabel"
+          >
             Nombre y Apellido
           </Form.Label>
           <Form.Control
@@ -14,11 +60,28 @@ const FormularioPerfilProf = () => {
             id="name"
             className="input px-0 border-start-0 border-end-0 border-top-0 border-bottom rounded-0"
             title="tu nombre y apellido"
-            disabled
+            disabled={habilitado}
+            {...register("fullname", {
+              required: "El nombre y apellido es obligatorio",
+              minLength: {
+                value: 3,
+                message:
+                  "El nombre y apellido debe tener como mínimo 3 caracteres",
+              },
+              maxLength: {
+                value: 50,
+                message:
+                  "El nombre y apellido debe tener como máximo 50 caracteres",
+              },
+            })}
           />
+          <div className="text-danger text-start">{errors.fullname?.message}</div>
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="email" className="text-md-center w-100 text-lg-start fw-bold mb-0 fontSizeLabel">
+          <Form.Label
+            htmlFor="email"
+            className="text-md-center w-100 text-lg-start fw-bold mb-0 fontSizeLabel"
+          >
             Email
           </Form.Label>
           <Form.Control
@@ -26,11 +89,22 @@ const FormularioPerfilProf = () => {
             id="email"
             className="input px-0 border-start-0 border-end-0 border-top-0 border-bottom rounded-0"
             title="tu dirección de correo electrónico"
-            disabled
+            disabled={habilitado}
+            {...register("mail", {
+              required: "El correo es obligatorio",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Ingrese una dirección de correo válida",
+              },
+            })}
           />
+          <div className="text-danger text-start">{errors.mail?.message}</div>
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="ubication" className="text-md-center w-100 text-lg-start fw-bold mb-0 fontSizeLabel">
+          <Form.Label
+            htmlFor="ubication"
+            className="text-md-center w-100 text-lg-start fw-bold mb-0 fontSizeLabel"
+          >
             Ubicación
           </Form.Label>
           <Form.Control
@@ -38,11 +112,24 @@ const FormularioPerfilProf = () => {
             id="ubication"
             className="input px-0 border-start-0 border-end-0 border-top-0 border-bottom rounded-0"
             title="tu ubicación actual"
-            disabled
+            disabled={habilitado}
+            {...register("address", {
+              minLength: {
+                value: 3,
+                message: "La ubicación debe tener como mínimo 3 caracteres",
+              },
+              maxLength: {
+                value: 70,
+                message: "La ubicación debe tener como máximo 70 caracteres",
+              },
+            })}
           />
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="wpp" className="text-md-center w-100 text-lg-start fw-bold mb-0 fontSizeLabel">
+          <Form.Label
+            htmlFor="wpp"
+            className="text-md-center w-100 text-lg-start fw-bold mb-0 fontSizeLabel"
+          >
             Número de WhatsApp
           </Form.Label>
           <Form.Control
@@ -50,37 +137,60 @@ const FormularioPerfilProf = () => {
             id="wpp"
             className="input px-0 border-start-0 border-end-0 border-top-0 border-bottom rounded-0"
             title="tu número de WhatsApp"
-            disabled
+            disabled={habilitado}
+            {...register("phone", {
+              pattern: /^[0-9]{10}$/,
+            })}
           />
         </Form.Group>
       </div>
       <div className="col-lg-5 mt-lg-5">
         <Form.Group className="mb-3 d-flex flex-column justify-content-center align-items-md-center gap-2">
-          <Form.Label htmlFor="wpp" className="text-md-center w-100 fw-bold mb-0 fontSizeLabel">
+          <Form.Label
+            htmlFor="wpp"
+            className="text-md-center w-100 fw-bold mb-0 fontSizeLabel"
+          >
             Foto de Perfil
           </Form.Label>
           <img
-            src={imgUsuario}
+            src={worker ? worker.img : ""}
             alt="imagen de perfil"
             className="border rounded-circle imgProfileForm"
             title="imagen de perfil"
           />
-          <Form.Control type="file" className="d-none" id="file" />
-          <button className="d-none btn rounded-2 btn-secondary px-4">
+          <Form.Control
+            type="file"
+            className="d-none"
+            id="file"
+            {...register("img")}
+          />
+          <button
+            className={` ${
+              habilitado ? "d-none" : "d-block"
+            } btn rounded-2 btn-secondary px-4`}
+          >
             Subir Imagen
           </button>
         </Form.Group>
       </div>
 
-      <div className="d-flex justify-content-end justify-content-md-center justify-content-lg-end d-block w-100">
-        <button type="submit" className="px-3 px-md-5 py-2 rounded-2 btnEdit text-white border-0">
-          Editar Mis datos
+      <div className="d-flex justify-content-end gap-2 justify-content-md-center justify-content-lg-end d-block w-100">
+        {!habilitado && (
+          <button
+            type="submit"
+            className="px-3 px-md-5 py-2 rounded-2 btn btn-secondary text-white border-0 opacity-75"
+          >
+            Cancelar
+          </button>
+        )}
+        <button
+          type="submit"
+          className="px-3 px-md-5 py-2 rounded-2 btnEdit text-white border-0"
+        >
+          {habilitado ? "Editar Mis datos" : "Guardar"}
         </button>
       </div>
     </Form>
-
-
-
   );
 };
 
--- src/components/profesional/MenuProfesional.jsx
+++ src/components/profesional/MenuProfesional.jsx
@@ -1,10 +1,49 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import image from "../../img/Logo.jpg";
 import { FaHome, FaUser, FaWpforms, FaListAlt } from "react-icons/fa";
 import { TbHelpCircleFilled } from "react-icons/tb";
 import { BiSolidExit } from "react-icons/bi";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../slice/authSlice";
+import Swal from "sweetalert2";
+import { fetchWorkerForID } from "../../slice/workersSlice";
+import { useEffect } from "react";
 
 const MenuProfesional = () => {
+  
+  const user = useSelector((state)=> state.auth.user);
+  const worker = useSelector((state) => state.workers.worker);
+  const status = useSelector((state) => state.workers.status);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchWorkerForID(user._id));
+    } else {
+      navigate("/");
+    }
+  }, [user]);
+
+   const cerrarSesion = () =>{
+    Swal.fire({
+      title: "Cerrar sesión",
+      text: "¿Estas seguro que deseas salir?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Cerrar sesión",
+      cancelButtonText: "cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(logout());
+        navigate("/");
+      }
+    });
+  }
+
   return (
     <nav className="navbar navbar-expand-md menuProfessional bg-body-tertiary d-flex align-items-start w-100 px-2 py-3">
       <div className="px-0 px-md-2 py-md-3 pt-lg-0  container-fluid containerMenu d-flex flex-md-column align-items-start">
@@ -15,13 +54,13 @@ const MenuProfesional = () => {
         </div>
         <figure className="d-none d-md-block text-center mx-auto w-100 mt-3 mt-md-5 mt-lg-3 mt-xl-5 mb-md-3 mb-lg-0 mb-xl-3">
           <img
-            src={image}
+            src={worker ? worker.img : ""}
             alt="imagen de perfil"
             title="imagen de perfil"
             className="rounded-circle shadow imgProfileUser"
           />
-          <figcaption className="fw-medium fs-5">Bruno Madozzo Romay</figcaption>
-          <figcaption>Tucumán, Argentina</figcaption>
+          <figcaption className="fw-medium fs-5">{worker ? worker.fullname : ""}</figcaption>
+          <figcaption>{worker ? worker.address : ""}</figcaption>
         </figure>
         <button
           className="navbar-toggler"
@@ -101,7 +140,7 @@ const MenuProfesional = () => {
                 </Link>
               </li>
               <li className="nav-item mt-xl-3 navItemOffCanvas text-start">
-                <button className="nav-link px-0 w-100 text-start d-flex gap-1 align-items-center navLink">
+                <button className="nav-link px-0 w-100 text-start d-flex gap-1 align-items-center navLink" onClick={cerrarSesion}>
                   <BiSolidExit className="fs-4 iconMenu" />
                   <span className="align-middle">Salir</span>
                 </button>
