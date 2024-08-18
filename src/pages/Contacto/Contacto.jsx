ab95a59235a26125c62120ed6c70a20e6a63fd87
Julian <julianser@hotmail.com.ar>
2024-08-18 20:02:21 -0300

    Se realizo parte del componente formulario

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import FormularioContacto from './components/FormularioContacto'
 
 const Contacto = () => {
   return (
-    <div className='mainSection'>
-      
-    </div>
+    <section className='mainSection container'>
+      <div className='my-5'>
+      <h1>¿En qué podemos Ayudarte?</h1>
+      <h2 className='my-3'>Hablemos por mail</h2>
+      </div>
+      <FormularioContacto/>
+    </section>
   )
 }
 
---
+++ src/pages/Contacto/components/FormularioContacto.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button, Form } from 'react-bootstrap'
+import '../contacto.css'
+const FormularioContacto = () => {
+  return (
+    <div className='w-50 border border-2 border-dark-subtle mb-5'>
+      <Form className='p-4'>
+    <h3>Contáctanos y pronto recibirás una respuesta</h3>
+      <Form.Group className="mb-3" controlId="formNombreApellido">
+        <Form.Label>Nombre y Apellido</Form.Label>
+        <Form.Control type="email" placeholder="Ej: Juan Perez" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Ej: juanperez@gmail.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formPassword">
+        <Form.Label>Mensaje</Form.Label>
+        <Form.Control as="textarea" rows={5} placeholder="Contraseña" />
+      </Form.Group>
+
+      <Button className='btnContacto border-0'type="submit">
+        Enviar
+      </Button>
+    </Form>
+    </div>
+  )
+}
+
+export default FormularioContacto
