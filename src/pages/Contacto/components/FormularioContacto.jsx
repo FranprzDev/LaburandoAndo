6344e346791385720000f61c639b36ba032a022e
Julian <julianser@hotmail.com.ar>
2024-08-18 20:32:24 -0300

    Se dio formato con prettier y se agrego el useForm de react hook form

--- src/pages/Contacto/components/FormularioContacto.jsx
+++ src/pages/Contacto/components/FormularioContacto.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
-import { Button, Form } from 'react-bootstrap'
-import '../contacto.css'
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import "../contacto.css";
+import { useForm } from "react-hook-form";
 const FormularioContacto = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  
   return (
-    <div className='border border-2 border-dark-subtle rounded-5 mb-5'>
-      <Form className='p-4'>
-    <h3>Contáctanos y pronto recibirás una respuesta</h3>
-      <Form.Group className="mb-3" controlId="formNombreApellido">
-        <Form.Label>Nombre y Apellido</Form.Label>
-        <Form.Control type="email" placeholder="Ej: Juan Perez" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Ej: juanperez@gmail.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formPassword">
-        <Form.Label>Mensaje</Form.Label>
-        <Form.Control as="textarea" rows={5} placeholder="Contraseña" />
-      </Form.Group>
-      <div className='text-end'>
-      <Button className='btnContacto w-25 border-0'type="submit">
-        Enviar
-      </Button>
-      </div>
-    </Form>
+    <div className="border border-2 border-dark-subtle rounded-5 mb-5">
+      <Form className="p-4">
+        <h3>Contáctanos y pronto recibirás una respuesta</h3>
+        <Form.Group className="mb-3" controlId="formNombreApellido">
+          <Form.Label>Nombre y Apellido</Form.Label>
+          <Form.Control type="email" placeholder="Ej: Juan Perez" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Ej: juanperez@gmail.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Label>Mensaje</Form.Label>
+          <Form.Control as="textarea" rows={5} placeholder="Contraseña" />
+        </Form.Group>
+        <div className="text-end">
+          <Button className="btnContacto w-25 border-0" type="submit">
+            Enviar
+          </Button>
+        </div>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default FormularioContacto
+export default FormularioContacto;
