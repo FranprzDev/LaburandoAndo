e42195725cb0dd620db94df46623fc7c48fbbcb5
brunomry <brunomadozzo@gmail.com>
2024-08-19 14:36:51 -0300

    corregi estructura de cp ItemPublicacion

--- src/domains/profesionales/components/ItemPublicacion.jsx
+++ src/domains/profesionales/components/ItemPublicacion.jsx
@@ -5,29 +5,19 @@ import { Link } from "react-router-dom";
 const ItemPublicacion = () => {
   return (
     <div className="cardPostProfessional shadow rounded-2 justify-content-center justify-content-md-between gy-2 align-items-center p-2 p-md-3 border">
-      <article className=" d-flex justify-content-center align-items-center justify-content-lg-start mt-0 ">
-        <p className="fw-bold align-middle mb-0 titlePostProfesional">
-          Clases particulares de inglés en línea para alumnos de primario y
-          secundario...
-        </p>
-      </article>
-      <article className="mt-lg-2">
-        <span className="mb-0">Profesor Particular</span>
-      </article>
-      <article className=" d-flex gap-3 align-items-center justify-content-end mt-0">
-        <Link
-          className="iconUpdatePost"
-          title="editar publicación"
-        >
-            <FaEdit/>
+      <p className="fw-bold align-middle mb-1 titlePostProfesional">
+        Clases particulares de inglés en línea para alumnos de primario y
+        secundario...
+      </p>
+      <span className="mb-0">Profesor Particular</span>
+      <div className=" d-flex gap-3 align-items-center justify-content-end mt-0">
+        <Link className="iconUpdatePost" title="editar publicación">
+          <FaEdit />
         </Link>
-        <a
-          className="iconDeletePost border-0 bg-none"
-          title="eliminar publicación"
-        >
-            <MdDelete/>
+        <a className="iconDeletePost" title="eliminar publicación">
+          <MdDelete />
         </a>
-      </article>
+      </div>
     </div>
   );
 };
