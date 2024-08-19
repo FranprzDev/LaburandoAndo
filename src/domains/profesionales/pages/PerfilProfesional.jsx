79975c9127da4c999dec95e6dce48b972e1680fa
Francisco Perez <franutn23@gmail.com>
2024-08-18 22:45:59 -0300

    Añado la lógica para el update del profesional, además hago algunos cambios de alertas y también pruebo el hook de Cloudinary

--- src/domains/profesionales/pages/PerfilProfesional.jsx
+++ src/domains/profesionales/pages/PerfilProfesional.jsx
@@ -1,25 +1,36 @@
 import FormularioPerfilProfesional from "../components/FormularioPerfilProfesional";
 import "../styles/perfilProfesional.css";
 import { useSelector } from "react-redux";
+import Loader from "../../../loaders/Loader";
+import { useEffect, useMemo, useState } from "react";
 
 const PerfilProfesional = () => { 
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    
+  }, [loading]);
   
   const user = useSelector((state) => state.auth.user);
 
   return (
-    <section className="container-fluid px-2 px-lg-5 py-5 mainSection containerProfile">
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item">
-            {user?.fullname}
-          </li>
-          <li className="breadcrumb-item text-primary">
-            Mi perfil
-          </li>
-        </ol>
-      </nav>
-      <FormularioPerfilProfesional />
-    </section>
+    <>
+      {loading && <Loader />}
+      <section className="container-fluid px-2 px-lg-5 py-5 mainSection containerProfile">
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              {user?.fullname}
+            </li>
+            <li className="breadcrumb-item text-primary">
+              Mi perfil
+            </li>
+          </ol>
+        </nav>
+        <FormularioPerfilProfesional loading={loading} setLoading={setLoading}/>
+      </section>
+    </>
   );
 };
 
