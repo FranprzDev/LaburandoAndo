a5fe8be53d907b8b4250b38572ea83888a4b2060
brunomry <brunomadozzo@gmail.com>
2024-08-20 20:15:16 -0300

    agregue cp AdminRouter con rutas de administrador

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import AuthRouter from "./AuthRouter";
 import ClientRouter from "./ClientRouter";
 import WorkerRouter from "./WorkerRouter";
+import AdminRouter from "./AdminRouter";
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
         <Route path="/*" element={<ClientRouter/>} />
         <Route path="/work/*" element={<WorkerRouter/>} />
         <Route path="/auth/*" element={<AuthRouter/>} />
+        <Route path="/admin/*" element={<AdminRouter />} />
     </Routes>
   );
 };
