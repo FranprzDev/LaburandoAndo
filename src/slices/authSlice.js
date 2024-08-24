819b2f868ec5c1ecc785856ef19ae113052d8339
Francisco Perez <franutn23@gmail.com>
2024-08-24 18:38:49 -0300

    Fix the Login & Refactor the Code of Slice & Actions

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -4,8 +4,8 @@ import { loginUser } from "./actions/authActions";
 
 const initialState = {
     user: JSON.parse(sessionStorage.getItem("usuarioLogeado")) || null,
-    stateSync: "idle",
     state: "idle",
+    isAuth: !!sessionStorage.getItem("usuarioLogeado"),
     role: "Client",
     error: null
 };
@@ -15,52 +15,41 @@ const authSlice = createSlice({
     initialState,
     reducers:{
         reset: (state) => {
-          state.stateSync = "idle";
           state.state = "idle";
           state.error = null;
         },
-        pending: (state) => {
-          state.stateSync = "cargando";
-        },
-        success: (state) => {
-          state.stateSync = "exitoso";
-        },
         error: (state, action) => {
-          state.stateSync = "error";
           state.error = action.payload;
         },
         logout: (state) => {
           state.state = "idle";
-          state.stateSync = "idle";
           state.user = null;
+          state.role = "Client";
+          state.isAuth = false;
           sessionStorage.removeItem("usuarioLogeado");
         },
     },
     extraReducers: (builder) =>{
         builder
         .addCase(loginUser.pending, (state) => {
-          state.stateSync = 'cargando';
           state.status = 'cargando';
         })
         .addCase(loginUser.fulfilled, (state, action) => {
-          state.stateSync = 'exitoso';
           state.status = 'exitoso';
           const decodedToken = jwtDecode(action.payload);
-
-          console.log(decodedToken)
-
+          state.isAuth = true;
           state.user = decodedToken.user;
           state.role = decodedToken.user.role;
           sessionStorage.setItem("usuarioLogeado", JSON.stringify(decodedToken.user));
         })
         .addCase(loginUser.rejected, (state, action) => {
-          state.stateSync = 'error';
           state.status = 'error';
+          state.isAuth = false;
           state.error = action.payload ? action.payload.message : action.error.message;
         });
     }
 })
 
-export const { reset, pending, success, error, logout } = authSlice.actions;
+export const { reset, error, logout } = authSlice.actions;
 
 export default authSlice.reducer;
--- src/slices/registerSlice.js
+++ src/slices/registerSlice.js
@@ -3,73 +3,81 @@ import instance from "../api/api";
 import { createClient, createProfessional } from "./actions/registerActions";
 
 const initialState = {
-    form: {
-      mail: "",
-      password: "",
-      fullname: "",
-      adress: "",
-      img: "",
-      authMethod: "Local",
-      type: "Client",
-    },
-    stateSync: "idle",
-    state: "idle",
-    error: null
+  form: {
+    mail: "",
+    password: "",
+    fullname: "",
+    adress: "",
+    img: "",
+    authMethod: "Local",
+    type: "Client",
+  },
+  state: "idle",
+  isCreated: false,
+  error: null,
 };
 
 const registerSlice = createSlice({
-    name: 'register',
-    initialState,
-    reducers:{
-      changeType: (state, action) => {
-            state.form.type = action.payload;
-      },
-      changePhoto: (state, action) => {
-         state.form.img = action.payload;
-      },
-      setValues: (state, action) => {
-        state.form.fullname = action.payload.fullname;
-        state.form.mail = action.payload.mail;
-        state.form.password = action.payload.password;
-      },
-      setAditionalValues : (state, action) => {
-        state.form.phone = action.payload.phone;
-        state.form.adress = action.payload.adress;
-      },
+  name: "register",
+  initialState,
+  reducers: {
+    changeType: (state, action) => {
+      state.form.type = action.payload;
     },
-    extraReducers: (builder) =>{
-        builder
-        .addCase(createProfessional.pending, (state) => {
-          state.stateSync = 'cargando';
-          state.status = 'cargando';
-        })
-        .addCase(createProfessional.fulfilled, (state, action) => {
-          state.stateSync = 'exitoso';
-          state.status = 'exitoso';
-          state.user = action.payload;
-        })
-        .addCase(createProfessional.rejected, (state, action) => {
-          state.stateSync = 'error';
-          state.status = 'idle';
-          state.error = action.payload ? action.payload.message : action.error.message;
-        })
-        .addCase(createClient.pending, (state) => {
-          state.stateSync = 'cargando';
-          state.status = 'cargando';
-        })
-        .addCase(createClient.fulfilled, (state, action) => {
-          state.stateSync = 'exitoso';
-          state.status = 'exitoso';
-          state.user = action.payload;
-        })
-        .addCase(createClient.rejected, (state, action) => {
-          state.stateSync = 'error';
-          state.status = 'idle';
-          state.error = action.payload ? action.payload.message : action.error.message;
-        })
-    }
-})
+    changePhoto: (state, action) => {
+      state.form.img = action.payload;
+    },
+    setValues: (state, action) => {
+      state.form.fullname = action.payload.fullname;
+      state.form.mail = action.payload.mail;
+      state.form.password = action.payload.password;
+    },
+    setAditionalValues: (state, action) => {
+      state.form.phone = action.payload.phone;
+      state.form.adress = action.payload.adress;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createProfessional.pending, (state) => {
+        state.status = "cargando";
+      })
+      .addCase(createProfessional.fulfilled, (state, action) => {
+        state.status = "exitoso";
+        state.isCreated = true;
+        state.user = action.payload;
+      })
+      .addCase(createProfessional.rejected, (state, action) => {
+        state.status = "idle";
+        state.isCreated = false;
+        state.error = action.payload
+          ? action.payload.message
+          : action.error.message;
+      })
+      .addCase(createClient.pending, (state) => {
+        state.status = "cargando";
+      })
+      .addCase(createClient.fulfilled, (state, action) => {
+        state.status = "exitoso";
+        state.user = action.payload;
+        state.isCreated = true;
+      })
+      .addCase(createClient.rejected, (state, action) => {
+        state.status = "idle";
+        state.isCreated = false;
+        state.error = action.payload
+          ? action.payload.message
+          : action.error.message;
+      });
+  },
+});
 
-export const { changeType, setValues, setAditionalValues, getProfessional, changePhoto } = registerSlice.actions;
+export const {
+  changeType,
+  setValues,
+  setAditionalValues,
+  getProfessional,
+  changePhoto,
+} = registerSlice.actions;
 
 export default registerSlice.reducer;
