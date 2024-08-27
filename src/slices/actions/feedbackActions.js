cc27a788bc3c80990acc5010e4bec8cc90ef0f2b
Julian <julianser@hotmail.com.ar>
2024-08-27 20:50:17 -0300

    Se completo el slice de feedback

---
+++ src/slices/actions/feedbackActions.js
@@ -0,0 +1,4 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import instance from "../../api/api";
+
+export const createFeedback = createAsyncThunk("work/createFeedback", async ({data, id}) => {})
--- src/slices/feedbackSlice.js
+++ src/slices/feedbackSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createFeedback } from "./actions/feedbackActions";
 
 const initialState = {
     feedbacks: [],
@@ -13,5 +14,16 @@ const feedbackSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
+        .addCase(createFeedback.pending,(state) => {
+            state.status = "idle"
+        })
+        .addCase(createFeedback.fulfilled,(state, action) => {
+            state.status= "Exitoso"
+            state.feedbacks= action.payload
+        })
+        .addCase(createFeedback.rejected,(state, action) => {
+            state.status= "Denegado"
+            state.error = action.error.message
+        })
     }
 })
