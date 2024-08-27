dbd53b934b1da9f0c64f060426d15553d7b76aee
brunomry <brunomadozzo@gmail.com>
2024-08-27 20:47:55 -0300

    agregue breadcrumb en cp Categorias de admin

--- src/domains/admin/pages/Categorias.jsx
+++ src/domains/admin/pages/Categorias.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Categorias = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className="container-fluid px-2 px-lg-5 py-3 py-md-5 mainSection containerProfile">
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">Admin</li>
+          <li className="breadcrumb-item text-primary">Categorías</li>
+        </ol>
+      </nav>
+
+    </section>
+  );
 };
 
 export default Categorias;
