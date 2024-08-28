b17acff1a2c3052b36cebb12c01833f7e095087d
Flavia Walther <flamwalther@gmail.com>
2024-08-28 12:25:34 -0300

    maquetando tarjetas con los datos de profesionales y clientes

--- src/domains/admin/components/ItemUsuario.jsx
+++ src/domains/admin/components/ItemUsuario.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const ItemUsuario = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="cardMessage shadow rounded-2 justify-content-center justify-content-md-between gy-3 align-items-center p-2 pt-md-3 px-md-3 border">
+      <div className="d-flex  justify-content-around">
+        <p className="fw-bold">nombre</p>
+        <p>mail </p>
+        <p>tipo de usuario</p>
+
+        <div className="d-flex  justify-content-center">
+          <button className="me-3">aa</button>
+          <button>bb</button>
         </div>
-    );
+      </div>
+
+      {/*      
+      <div className="d-flex  justify-content-around">
+        <p className="fw-bold">nombre</p>
+        <p>mail </p>
+        <p>tipo de usuario</p>
+      </div>
+      <div className="d-flex  justify-content-center">
+        <button className="me-3">aa</button>
+        <button>bb</button>
+      </div> */}
+    </div>
+  );
 };
 
 export default ItemUsuario;
--- src/domains/admin/components/ListaUsuarios.jsx
+++ src/domains/admin/components/ListaUsuarios.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ItemUsuario from "./ItemUsuario";
 
 const ListaUsuarios = () => {
   return (
     <div className="gap-3 align-items-start d-flex flex-column containerMessages w-100 mt-2">
-      {/* <ItemMensaje></ItemMensaje> */}
+      <ItemUsuario />
     </div>
   );
 };
