95ca10bd747d8b93ae6211b4267fb293500862f8
Flavia Walther <flamwalther@gmail.com>
2024-08-28 17:27:19 -0300

    se trabaja en estilos

--- src/domains/admin/components/ItemUsuario.jsx
+++ src/domains/admin/components/ItemUsuario.jsx
@@ -13,9 +13,9 @@ const ItemUsuario = () => {
       la page que llama a este componenete*/}
 
       <div className="row ms-1 cardMessage shadow rounded-2 p-2 pt-md-3 px-md-3 border">
-        <div className="col-9 d-flex flex-column flex-md-row justify-content-around">
-          <p className="fw-bold mb-0 ">nombre</p>
-          <p className="mb-0">mail</p>
+        <div className="col-9 d-flex flex-column flex-md-row justify-content-start ">
+          <p className="fw-bold mb-0 me-5">nombre y apellido</p>
+          <p className="mb-0">email</p>
         </div>
         <div className="col-3 d-flex justify-content-end">
           <button className="btn btn-warning me-3">
