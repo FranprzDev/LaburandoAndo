62c049cdb1ef113d176ca84c15c08928aeb801c0
Julian <julianser@hotmail.com.ar>
2024-08-28 19:37:03 -0300

    Se realizo el read de las categorias

--- src/domains/admin/components/ItemCategoria.jsx
+++ src/domains/admin/components/ItemCategoria.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { MdDelete } from "react-icons/md";
 
-const ItemCategoria = () => {
+const ItemCategoria = ({categoria}) => {
   return (
     <div className="cardCategory shadow rounded-2 d-flex flex-column px-2 p-md-2 flex-md-row justify-content-center justify-content-md-between gy-3 align-items-center px-md-3 border">
       <p className="text-center mb-0 text-md-start fw-bold w-100 d-block p-2 rounded-2 align-middle">
-        Profesor particular
+        {categoria.name}
       </p>
       <div className="d-flex justify-content-end ">
         <button className=" btn py-0 py-md-1">
--- src/domains/admin/components/ListaCategorias.jsx
+++ src/domains/admin/components/ListaCategorias.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ItemCategoria from "./ItemCategoria";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCategories } from "../../../slices/actions/categoryActions";
 
 const ListaCategorias = () => {
+
+  const categorias = useSelector((state) => state.categories.categories)
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    dispatch(fetchCategories())
+  }, [])
+
+  console.log(categorias)
+  
+
   return (
     <div className="gap-2 align-items-start d-flex flex-column containerMessages w-100 ">
-      <ItemCategoria></ItemCategoria>
-      <ItemCategoria></ItemCategoria>
+      {categorias.map((categoria => (<ItemCategoria categoria={categoria} key={categoria._id}></ItemCategoria>)))}
+      
+      
 
       {/* <div className="mt-5 text-center border px-3 py-5 p-md-5 rounded-2 shadow">
           <p className="mb-4">No se agregó ninguna categoría</p>
