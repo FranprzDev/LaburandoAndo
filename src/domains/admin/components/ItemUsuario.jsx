8b3eaa51b700d72b828a4cd1e8d5153b82325deb
Flavia Walther <flamwalther@gmail.com>
2024-08-28 17:06:29 -0300

    se trabaja en la organizacion de la info dentro de la card

--- src/domains/admin/components/ItemUsuario.jsx
+++ src/domains/admin/components/ItemUsuario.jsx
@@ -5,13 +5,19 @@ import { RiEdit2Fill } from "react-icons/ri";
 
 const ItemUsuario = () => {
   return (
-    <div className="cardMessage shadow rounded-2 justify-content-center justify-content-md-between gy-3 align-items-center p-2 pt-md-3 px-md-3 border">
-      <div className="d-flex  justify-content-around">
-        <p className="fw-bold">nombre</p>
-        <p>mail </p>
-        <p>tipo de usuario</p>
+    <>
+      {/* el div a continuacion se debe repetir por cada profesional/cliente 
+      se debe filtrar el tipo de usuario (profesional o cliente) 
+      traer nombre y email, y agregarlo en la etiqueta p correspondiente 
+      El tipo de usuario no es necesario listarlo en la card porque esta como titulo en 
+      la page que llama a este componenete*/}
 
-        <div className="d-flex  justify-content-center">
+      <div className="row ms-1 cardMessage shadow rounded-2 p-2 pt-md-3 px-md-3 border">
+        <div className="col-9 d-flex flex-column flex-md-row justify-content-around">
+          <p className="fw-bold mb-0 ">nombre</p>
+          <p className="mb-0">mail</p>
+        </div>
+        <div className="col-3 d-flex justify-content-end">
           <button className="btn btn-warning me-3">
             <RiEdit2Fill className="fs-5" />
           </button>
@@ -20,18 +26,7 @@ const ItemUsuario = () => {
           </button>
         </div>
       </div>
-
-      {/*      
-      <div className="d-flex  justify-content-around">
-        <p className="fw-bold">nombre</p>
-        <p>mail </p>
-        <p>tipo de usuario</p>
-      </div>
-      <div className="d-flex  justify-content-center">
-        <button className="me-3">aa</button>
-        <button>bb</button>
-      </div> */}
-    </div>
+    </>
   );
 };
 
