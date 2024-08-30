520ea7be9e5a6d454bea9540d609f4aa974a8bad
Julian <julianser@hotmail.com.ar>
2024-08-30 19:38:04 -0300

    Se realizaron mediaqueries en un css propio para adaptar el tamaño de la imagen

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,11 +1,11 @@
 import { Button } from 'react-bootstrap'
 import Error from '../../img/Error404.jpeg'
-
+import './Error404.css'
 const Error404 = () => {
   return (
-    <section className='text-center my-5'>
+    <section className='container text-center my-5'>
     <div>
-        <img src={Error} className='img-fluid' alt='imagen error 404' />
+        <img className='imgError' src={Error} alt='imagen error 404' />
     </div>
     <h2 className='fst-italic fw-bold'>"Parece que tu pala no encontró la página que buscabas. ¡A seguir cavando en el lugar correcto!"</h2>
     <Button className='btn btn-dark'>Volver al Inicio</Button>
