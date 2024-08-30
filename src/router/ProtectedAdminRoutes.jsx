4d49579f6e05b81afca461fa435b197b2c944b5a
Julian Martin <75331840+Juliancito1@users.noreply.github.com>
2024-08-30 02:31:41 -0300

    Se realizo la Proteccion de las Rutas Worker y Admin (#87)
    * Se realizo la proteccion de ruta del admin
    
    * Se realizo la proteccion de ruta de profesional
    
    * Se dio formato con prettier
    
    * Se quitaron los alert para no dejar indicios de que se tratan de rutas protegidas

---
+++ src/router/ProtectedAdminRoutes.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const ProtectedAdminRoutes = ({ children }) => {
+  const usuario = JSON.parse(sessionStorage.getItem("usuarioLogeado")) || null;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!usuario || usuario.role !== "admin") {
+      navigate("/");
+    }
+  }, [usuario, navigate]);
+
+  if (usuario && usuario.role === "admin") {
+    return children;
+  } else {
+    return null;
+  }
+};
+
+export default ProtectedAdminRoutes;
---
+++ src/router/ProtectedWorkerRoutes.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const ProtectedWorkerRoutes = ({ children }) => {
+  const usuario = JSON.parse(sessionStorage.getItem("usuarioLogeado")) || null;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!usuario || usuario.role !== "worker") {
+      navigate("/");
+    }
+  }, [usuario, navigate]);
+
+  if (usuario && usuario.role === "worker") {
+    return children;
+  } else {
+    return null;
+  }
+};
+
+export default ProtectedWorkerRoutes;
