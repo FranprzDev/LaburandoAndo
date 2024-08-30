22c830ef70354264b141c5d616ebec486bdccfbb
Julian <julianser@hotmail.com.ar>
2024-08-30 19:20:15 -0300

    Se agrego texto y boton al error404

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,10 +1,15 @@
+import { Button } from 'react-bootstrap'
 import Error from '../../img/Error404.jpeg'
 
 const Error404 = () => {
   return (
+    <section className='text-center my-5'>
     <div>
-        <img src={Error} alt="" />
+        <img src={Error} className='img-fluid' alt='imagen error 404' />
     </div>
+    <h2 className='fst-italic fw-bold'>"Parece que tu pala no encontró la página que buscabas. ¡A seguir cavando en el lugar correcto!"</h2>
+    <Button className='btn btn-dark'>Volver al Inicio</Button>
+    </section>
   )
 }
 
