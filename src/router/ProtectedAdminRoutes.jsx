52413667f90191f00081b05c4d5de5beccdf79a4
Julian <julianser@hotmail.com.ar>
2024-08-30 01:06:01 -0300

    Se quitaron los alert para no dejar indicios de que se tratan de rutas protegidas

--- src/router/ProtectedAdminRoutes.jsx
+++ src/router/ProtectedAdminRoutes.jsx
@@ -1,17 +1,14 @@
 import { useNavigate } from "react-router-dom";
-import useAlert from "../hooks/useAlertHook";
 import { useEffect } from "react";
 
 const ProtectedAdminRoutes = ({ children }) => {
   const usuario = JSON.parse(sessionStorage.getItem("usuarioLogeado")) || null;
-  const { autoCloseAlert } = useAlert();
   const navigate = useNavigate();
   useEffect(() => {
     if (!usuario || usuario.role !== "admin") {
-      autoCloseAlert("No tienes permisos para acceder a esta página", "error");
       navigate("/");
     }
-  }, [usuario, navigate, autoCloseAlert]);
+  }, [usuario, navigate]);
 
   if (usuario && usuario.role === "admin") {
     return children;
--- src/router/ProtectedWorkerRoutes.jsx
+++ src/router/ProtectedWorkerRoutes.jsx
@@ -1,20 +1,14 @@
 import { useNavigate } from "react-router-dom";
-import useAlert from "../hooks/useAlertHook";
 import { useEffect } from "react";
 
 const ProtectedWorkerRoutes = ({ children }) => {
   const usuario = JSON.parse(sessionStorage.getItem("usuarioLogeado")) || null;
-  const { autoCloseAlert } = useAlert();
   const navigate = useNavigate();
   useEffect(() => {
     if (!usuario || usuario.role !== "worker") {
-      autoCloseAlert(
-        "No estas logueado o no eres un profesional para acceder a esta página",
-        "error"
-      );
       navigate("/");
     }
-  }, [usuario, navigate, autoCloseAlert]);
+  }, [usuario, navigate]);
 
   if (usuario && usuario.role === "worker") {
     return children;
