ba8d233ca2d957bb0809e76082c37ede5d98c9d1
Flavia Walther <flamwalther@gmail.com>
2024-09-01 20:12:15 -0300

    se añade contenido a la pagina de Politica de privacidad

--- src/pages/TerminosCondiciones/PoliticaPrivacidad.jsx
+++ src/pages/TerminosCondiciones/PoliticaPrivacidad.jsx
@@ -2,8 +2,88 @@ import React from "react";
 
 const PoliticaPrivacidad = () => {
   return (
-    <div className="container-fluid">
-      <div className="row">PoliticaPrivacidad</div>
+    <div className="card">
+      <div className="card-body">
+        <p className="mx-5 my-5">
+          <strong>Política de Privacidad</strong> <br />
+          <br />
+          <strong>Última actualización:</strong> 01/09/2024 <br />
+          <br />
+          Esta Política de Privacidad describe cómo LaburandoAndo recopila, usa,
+          comparte y protege la información personal de los usuarios que visitan
+          o utilizan nuestro sitio web y los servicios que ofrecemos para
+          conectar a profesionales independientes con clientes. <br />
+          <br />
+          <strong>1. Información que Recopilamos</strong> <br />
+          Recopilamos información personal de los usuarios en las siguientes
+          categorías: <br />
+          • Datos de registro: nombre, dirección de correo electrónico, y otra
+          información proporcionada al crear una cuenta.
+          <br />
+          • Información de perfil: fotos, descripciones personales, habilidades,
+          experiencia, y otra información proporcionada por los profesionales
+          independientes y clientes. <br />
+          <br />
+          <strong>2. Cómo Usamos la Información </strong>
+          <br />
+          Utilizamos la información recopilada para los siguientes propósitos:
+          <br />
+          • Proveer nuestros servicios: conectar a profesionales independientes
+          con clientes. <br />
+          • Mejorar nuestro Sitio: analizar y comprender cómo los usuarios
+          interactúan con nuestro Sitio para mejorarlo continuamente. <br />
+          • Comunicación: responder a consultas de soporte. <br />
+          • Cumplimiento legal: cumplir con las obligaciones legales, resolver
+          disputas y hacer cumplir nuestros términos y condiciones. <br />
+          <br />
+          <strong>3. Cómo Compartimos la Información</strong>
+          <br />
+          No vendemos ni alquilamos tu información personal a terceros. Sin
+          embargo, podemos compartir tu información en las siguientes
+          circunstancias:
+          <br />
+          • Con otros usuarios: la información de perfil de los profesionales
+          independientes se comparte con los clientes potenciales y viceversa,
+          como parte del servicio de conexión.
+          <br />
+          • Obligaciones legales: cuando sea requerido por ley, para cumplir con
+          procesos legales, o para proteger los derechos y seguridad de nuestros
+          usuarios y la Empresa. <br />
+          <br />
+          <strong>4. Seguridad de la Información</strong>
+          <br /> Implementamos medidas de seguridad razonables para proteger tu
+          información contra el acceso no autorizado, uso indebido o
+          divulgación. Sin embargo, ninguna transmisión de datos por Internet es
+          completamente segura, por lo tanto, no podemos garantizar la seguridad
+          absoluta. <br />
+          <br />
+          <strong>5. Retención de Datos</strong>
+          <br /> Conservamos tu información personal solo durante el tiempo que
+          sea necesario para cumplir con los fines descritos en esta Política de
+          Privacidad, a menos que la ley requiera un período de retención más
+          largo.
+          <br />
+          <br />
+          <strong>6. Tus Derechos</strong> <br />
+          Dependiendo de tu jurisdicción, puedes tener ciertos derechos sobre tu
+          información personal, tales como:
+          <br />
+          • Acceso a la información que hemos recopilado sobre ti. <br />
+          • Solicitar la corrección de datos incorrectos. <br />
+          • Solicitar la eliminación de tu información personal. <br />
+          • Oponerse al procesamiento de tus datos personales o restringir su
+          uso. <br />
+          <br />
+          <strong>7. Cambios en la Política de Privacidad </strong>
+          <br />
+          Nos reservamos el derecho de actualizar esta Política de Privacidad en
+          cualquier momento. Notificaremos cualquier cambio publicando la nueva
+          versión en nuestro Sitio con la fecha de la última actualización. 8.
+          Contacto Si tienes preguntas o inquietudes sobre esta Política de
+          Privacidad, por favor contáctanos a través del link de “Contacto” al
+          pie de la página.
+        </p>
+      </div>
     </div>
   );
 };
