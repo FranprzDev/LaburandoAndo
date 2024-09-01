e1c64c7f03ca5c3f406171e02a7849e99a090b01
Flavia Walther <flamwalther@gmail.com>
2024-09-01 20:38:06 -0300

    se añade contenido a la pagina de Condiciones de Usos para Profesionales

--- src/pages/TerminosCondiciones/UsoCliente.jsx
+++ src/pages/TerminosCondiciones/UsoCliente.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const UsoCliente = () => {
-  return <div>UsoCliente</div>;
+  return (
+    <div className="card">
+      <div className="card-body">
+        <p className="mx-5 my-5"></p>
+      </div>
+    </div>
+  );
 };
 
 export default UsoCliente;
