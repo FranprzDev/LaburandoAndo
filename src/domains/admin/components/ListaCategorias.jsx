bbe58f8dac2ec7e652f28ae3bfe4d5c9f2502300
CosmeNicolas <nicomas7590@gmail.com>
2024-09-02 19:22:04 -0300

    agregue el loader en el admin en la seccion de listar los usuarios

--- src/domains/admin/components/ListaCategorias.jsx
+++ src/domains/admin/components/ListaCategorias.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import ItemCategoria from "./ItemCategoria";
 import { useSelector } from "react-redux";
-
 const ListaCategorias = () => {
-
-  const category = useSelector((state) => state.categories.categories)
+  const category = useSelector((state) => state.categories.categories);
 
   return (
     <div className="d-flex justify-content-center flex-column gap-2 flex-md-row flex-wrap">
-      {
-        category.length === 0 ? (
-          <div className="mt-5 text-center border px-3 py-5 p-md-5 rounded-2 shadow">
-              <p className="mb-4">No hay categorías que mostrar.</p>
-          </div>
-        ) : (
-          category.map((categoria) => (
-            <ItemCategoria key={categoria._id} categoria={categoria} />
-          ))
-        )
-      }
+      {category.length === 0 ? (
+        <div className="mt-5 text-center border px-3 py-5 p-md-5 rounded-2 shadow">
+          <p className="mb-4">No hay categorías que mostrar.</p>
+        </div>
+      ) : (
+        category.map((categoria) => (
+          <ItemCategoria key={categoria._id} categoria={categoria} />
+        ))
+      )}
     </div>
   );
 };
