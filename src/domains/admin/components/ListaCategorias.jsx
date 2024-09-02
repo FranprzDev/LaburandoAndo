e4cefdf417954e3ae289769e91d7c4411c328cd5
Bruno Madozzo <brunomadozzo@gmail.com>
2024-09-02 02:17:03 -0300

    agregue estilos css generales para etiquetas de texto para que ningun texto desborde su contenedor y alto minimo para contenedor con paginacion de pagina Soporte Admin. Centre categorias en admin solo para dispositvos > 1400

--- src/domains/admin/components/ListaCategorias.jsx
+++ src/domains/admin/components/ListaCategorias.jsx
@@ -3,22 +3,19 @@ import ItemCategoria from "./ItemCategoria";
 import { useSelector } from "react-redux";
 
 const ListaCategorias = () => {
-
-  const category = useSelector((state) => state.categories.categories)
+  const category = useSelector((state) => state.categories.categories);
 
   return (
-    <div className="d-flex justify-content-center flex-column gap-2 flex-md-row flex-wrap">
-      {
-        category.length === 0 ? (
-          <div className="mt-5 text-center border px-3 py-5 p-md-5 rounded-2 shadow">
-              <p className="mb-4">No hay categorías que mostrar.</p>
-          </div>
-        ) : (
-          category.map((categoria) => (
-            <ItemCategoria key={categoria._id} categoria={categoria} />
-          ))
-        )
-      }
+    <div className="d-flex justify-content-xxl-center flex-column gap-2 flex-md-row flex-wrap">
+      {category.length === 0 ? (
+        <div className="mt-5 text-center border px-3 py-5 p-md-5 rounded-2 shadow">
+          <p className="mb-4">No hay categorías que mostrar.</p>
+        </div>
+      ) : (
+        category.map((categoria) => (
+          <ItemCategoria key={categoria._id} categoria={categoria} />
+        ))
+      )}
     </div>
   );
 };
